ddbfebdd765c13e7e81caa7f393665a47330b91f
Jacob <jerling1989@gmail.com>
2018-04-26 21:16:17 -0400

    getting input from user

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ const _ = require('lodash');
 // REQUIRE FILES
 const notes = require('./notes.js');
 
+
+var command = process.argv[2];
+console.log('Command:', command);
+
+
+if (command === 'add') {
+	console.log('Adding new note');
+} else if (command === 'list') {
+	console.log('Listing all notes');
+} else if (command === 'read') {
+	console.log('Reading note');
+} else if (command === 'remove') {
+	console.log('Removing note')
+} else {
+	console.log('Command not recognized');
+}
