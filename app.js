47a646bea286d81099760cd94f7e9e1503d99b82
Jacob <jerling1989@gmail.com>
2018-04-26 20:36:33 -0400

    filteredArray

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(-6, 12));
+var filteredArray = _.uniq(['Jacob', 1, 'Jacob', 'Frank', 1, 2, 3, 4, 4]);
+console.log(filteredArray);
+
+// console.log(_.isString(true));
+// console.log(_.isString('Jacob'));
+
+// console.log('Result:', notes.add(-6, 12));
 
 // var user = os.userInfo();
 // fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
