d417a3991b88c7294b209378c77e53e32d5285c3
Jacob <jerling1989@gmail.com>
2018-04-26 20:50:04 -0400

    get files ready

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 console.log('Starting app.js');
 
+// REQUIRE NPM PACKAGES
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+// REQUIRE FILES
 const notes = require('./notes.js');
 
-var filteredArray = _.uniq(['Jacob', 1, 'Jacob', 'Frank', 1, 2, 3, 4, 4]);
-console.log(filteredArray);
-
-// console.log(_.isString(true));
-// console.log(_.isString('Jacob'));
-
-// console.log('Result:', notes.add(-6, 12));
-
-// var user = os.userInfo();
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
--- notes.js
+++ notes.js
@@ -1,10 +1,7 @@
 console.log('Starting notes.js');
 
+// NOTE.ADDNOTE() FUNCTION
 module.exports.addNote = () => {
 	console.log('addNote');
 	return 'New note';
-};
-
-module.exports.add = (a, b) => {
-	return a + b;
 };
